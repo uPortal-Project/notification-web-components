e4da45eadf209fc41e4a857599a6b479014ecdf3
Ryan Mathis <rmathis@unicon.net>
2019-03-18 13:52:14 -0700

    Initial project setup

---
+++ @uportal-notify/notification-list/src/components/NotificationList.vue
@@ -0,0 +1,28 @@
+<template>
+  <span>Hello from the Notification List</span>
+</template>
+<script>
+import oidc from '@uportal/open-id-connect';
+
+export default {
+  name: 'notification-list',
+  data() {
+    return {
+    };
+  },
+  computed: { },
+  props: {
+    oidcUrl: {
+      type: String,
+      default: '/uPortal/api/v5-1/userinfo'
+    }
+  },
+  methods: { },
+  mounted() { }
+};
+</script>
+<style lang="scss" scoped>
+.notification-list-container /deep/ {
+  @import '../../node_modules/bootstrap/scss/bootstrap';
+}
+</style>
