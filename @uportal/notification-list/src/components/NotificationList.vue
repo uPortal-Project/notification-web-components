0eeff7538b5e8b5d8e8e4e2ef255eeacc2098d6f
Ryan Mathis <rmathis@unicon.net>
2019-03-27 07:43:10 -0700

    chore: set package properties for publishing

--- @uportal/notification-list/src/components/NotificationList.vue
+++ @uportal/notification-list/src/components/NotificationList.vue
@@ -1,75 +1,21 @@
 <template>
-  <div class="notification-list-container">
-    Hello from the Notification List
-  </div>
+    <div class="notification-list-container">
+        Hello from the Notification List
+    </div>
 </template>
 <script>
 // import oidc from '@uportal/open-id-connect';
 
 export default {
-  name: 'notification-list',
-  data() {
-    return {};
-  },
-  computed: {},
-  props: {
-    oidcUrl: {
-      type: String,
-      default: '/uPortal/api/v5-1/userinfo'
+    name: 'notification-list',
+    data() {
+        return {};
     },
-    dataUrl: {
-      type: String,
-      required: true
-    }
-  },
-  methods: {
-    handleOidcError(err) {
-      this.hasError = true;
-      this.errorMessage = 'There was a problem authorizing this request.';
-    },
-    handleWflError(err) {
-      this.hasError = true;
-      this.errorMessage = 'There was a problem retrieving notifications.';
-    },
-    async getToken() {
-      try {
-        return await oidc({ userInfoApiUrl: this.oidcUrl });
-      } catch (err) {
-        console.error(err);
-        this.handleOidcError(err);
-      }
-    },
-    async fetchNotificationData() {
-      const token = this.debug ? null : (await this.getToken()).encoded;
-
-      try {
-        const response = await fetch(this.url, {
-          credentials: 'same-origin',
-          headers: {
-            Authorization: 'Bearer ' + token,
-            'content-type': 'application/jwt'
-          }
-        });
-
-        if (!response.ok || response.status !== 200) {
-          throw new Error(response.statusText);
-        }
-
-        const payload = await response.json();
-
-        console.log(payload);
-      } catch (err) {
-        console.error(err);
-      }
-    }
-  },
-  mounted() {
-    this.fetchNotificationData();
-  }
+    computed: {}
 };
 </script>
 <style lang="scss" scoped>
 .notification-list-container /deep/ {
-  @import '../../node_modules/bootstrap/scss/bootstrap';
+    @import '../../node_modules/bootstrap/scss/bootstrap';
 }
 </style>
